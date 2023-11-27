cdfe20f05cc5593b233e700475c0d665156c8903
TboxAndYaqian <2362691862@qq.com>
2023-11-27 08:54:27 +0800

    lxp 使用策略模式决定调用函数，避免switch

--- CesiumDemo2/src/module/Class_Listening.js
+++ CesiumDemo2/src/module/Class_Listening.js
@@ -9,21 +9,24 @@ export function selectedEntityTree(selectedEntity) {
     // 当选中节点为空时直接返回
     if(!isListrening){return}
     if(typeof selectedEntity == "undefined"){ return}
-    switch(selectedEntity.name){
-        case "WindPoint":
-            console.log(selectedEntity);
-        break;
-        case "rainfallPoint":
-            console.log(selectedEntity);
-        break;
-        case "MovePoint":
-            console.log(selectedEntity);
-        break;
-        case "humiturePoint":
-            console.log(selectedEntity);
-        break;
-        default :
-            console.log("unknowname");
-            break;
+    strategies[selectedEntity.name](selectedEntity)
+}
+
+var strategies = {  
+    'WindPoint': function(Entity) {
+        console.log(Entity);
+        return Entity;  
+    },  
+    'rainfallPoint': function(Entity) {  
+        console.log(Entity);
+        return Entity;  
+    },  
+    'MovePoint': function(Entity) {  
+        console.log(Entity);
+        return Entity;  
+    },
+    'humiturePoint':function(Entity) {  
+        console.log(Entity);
+        return Entity;  
     }
 }
